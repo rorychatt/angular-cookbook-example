bede93fa514d10ce09f2349c5d96ff1375260a9d
rorychatt <pavel.rinne@gmail.com>
2024-10-16 14:27:49 +0200

    feat: add c u functionality to items store service

--- src/app/services/items-store.service.ts
+++ src/app/services/items-store.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { Basket } from '../models';
+import { Basket, Item } from '../models';
 import { ComponentStore } from '@ngrx/component-store';
 
 @Injectable({
@@ -10,4 +10,10 @@ export class ItemsStoreService extends ComponentStore<Basket> {
   constructor() {
     super({ items: [] });
   }
+
+  readonly items$ = this.select(state => state.items);
+
+  readonly addItem = this.updater((state, item: Item) => ({
+    items: [...state.items, item],
+  }));
 }
