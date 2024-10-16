305b06919abfdb43f333e78ba3ea37b44b62ae1d
Lea Hedberg Segeholm <lea.hedberg.segeholm@scania.com>
2024-10-16 14:18:23 +0200

    info: added additem function

--- src/app/components/select-items/select-items.component.ts
+++ src/app/components/select-items/select-items.component.ts
@@ -8,5 +8,8 @@ import { Component } from '@angular/core';
   styleUrl: './select-items.component.scss'
 })
 export class SelectItemsComponent {
+addItem() {
+throw new Error('Method not implemented.');
+}
 
 }
