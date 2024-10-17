3f778f31a80ef1835ce5bd2f7f7ae12c436a2d21
rorychatt <pavel.rinne@gmail.com>
2024-10-17 10:26:45 +0200

    feat: use angular forms to input data

--- src/app/models/index.ts
+++ src/app/models/index.ts
@@ -1,2 +1,3 @@
 export * from './item';
 export * from './basket';
+export * from './possibleItems';
---
+++ src/app/models/possibleItems.ts
@@ -0,0 +1,6 @@
+export const possibleItems = [
+    'Apple 🍎',
+    'Banana 🍌',
+    'Cherry 🍒',
+    'Grape 🍇'
+]
