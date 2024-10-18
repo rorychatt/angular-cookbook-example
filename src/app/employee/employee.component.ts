5ef1c7dbc54c51089e64265a1ebf18e7a8588156
rorychatt <pavel.rinne@gmail.com>
2024-10-18 09:30:53 +0200

    feat: add basket to employee page

--- src/app/employee/employee.component.ts
+++ src/app/employee/employee.component.ts
@@ -1,12 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { SelectItemsComponent } from "../components/select-items/select-items.component";
+import { Basket } from '../models';
+import { ItemsStoreService } from '../services/items-store.service';
+import { AsyncPipe } from '@angular/common';
 
 @Component({
   selector: 'app-employee',
   standalone: true,
-  imports: [],
+  imports: [SelectItemsComponent, AsyncPipe],
   templateUrl: './employee.component.html',
   styleUrl: './employee.component.scss'
 })
 export class EmployeeComponent {
+
+  basket: Basket = { items: [] };
+  itemsStoreService = inject(ItemsStoreService);
 
 }
