88a7b36648ede5227bce19858386a02618d7e075
Lea Hedberg Segeholm <lea.hedberg.segeholm@scania.com>
2024-10-18 14:00:12 +0200

    feat: added logout button

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, input } from '@angular/core';
 import { RouterLink } from '@angular/router';
+
 
 @Component({
   selector: 'app-header',
@@ -9,5 +10,5 @@ import { RouterLink } from '@angular/router';
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
-
+  isLoggedIn = input<boolean>(false)
 }
