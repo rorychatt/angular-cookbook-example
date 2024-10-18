065518f766dde704b34b657a656613d7e48ac993
rorychatt <pavel.rinne@gmail.com>
2024-10-18 14:29:41 +0200

    feat: create login service

---
+++ src/app/services/login.service.ts
@@ -0,0 +1,19 @@
+import {Injectable} from '@angular/core';
+import {ComponentStore} from '@ngrx/component-store';
+import {Authority} from '../models';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LoginService extends ComponentStore<Authority> {
+
+  constructor() {
+    super({isLoggedIn: false});
+  }
+
+  readonly loggedIn$ = this.select((state) => state.isLoggedIn);
+
+  readonly logIn = this.updater((state) => ({isLoggedIn: true}));
+
+  readonly logOut = this.updater((state) => ({isLoggedIn: false}));
+}
