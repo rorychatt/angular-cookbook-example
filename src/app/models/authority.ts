c030cb69c2e2d4a0901f3f4820e19e83b91ff8ec
rorychatt <pavel.rinne@gmail.com>
2024-10-19 13:44:07 +0200

    info: refactor Authority into authState

--- src/app/models/authority.ts
+++
@@ -1,3 +0,0 @@
-export type Authority = {
-  isLoggedIn: boolean;
-}
--- src/app/services/login.service.ts
+++ src/app/services/login.service.ts
@@ -1,11 +1,11 @@
 import { Injectable } from '@angular/core';
 import { ComponentStore } from '@ngrx/component-store';
-import { Authority } from '../models';
+import { AuthState } from '../models';
 
 @Injectable({
   providedIn: 'root'
 })
-export class LoginService extends ComponentStore<Authority> {
+export class LoginService extends ComponentStore<AuthState> {
 
   constructor() {
     super({ isLoggedIn: false });
@@ -13,7 +13,7 @@ export class LoginService extends ComponentStore<Authority> {
 
   readonly loggedIn$ = this.select((state) => state.isLoggedIn);
 
-  readonly logIn = this.updater((state) => ({ isLoggedIn: true }));
+  readonly logIn = this.updater((_) => ({ isLoggedIn: true }));
 
-  readonly logOut = this.updater((state) => ({ isLoggedIn: false }));
+  readonly logOut = this.updater((_) => ({ isLoggedIn: false }));
 }
