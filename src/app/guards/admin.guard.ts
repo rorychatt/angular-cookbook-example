b9d651157086961056f87225a8bf8f071b19d9a9
rorychatt <pavel.rinne@gmail.com>
2024-10-22 08:45:20 +0200

    info: temp commit

--- src/app/guards/admin.guard.ts
+++ src/app/guards/admin.guard.ts
@@ -6,7 +6,17 @@ export const adminGuard: CanActivateFn = (route, state) => {
 
   const authService = inject(LoginService);
 
-
+  // let isLoggedIn: boolean;
+  // let isAdmin: boolean;
+  //
+  //
+  // authService.loggedIn$.subscribe(state => {
+  //   isLoggedIn = state;
+  // })
+  //
+  // authService.isAdmin$.subscribe(state => {
+  //   isAdmin = state;
+  // })
 
   return true;
 };
