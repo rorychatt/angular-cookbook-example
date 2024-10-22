d2c21d0f3ed11a235bed800425fb454dc9f6c1fc
rorychatt <pavel.rinne@gmail.com>
2024-10-22 12:10:07 +0200

    feat: add redirect to main page

--- src/app/guards/admin.guard.ts
+++ src/app/guards/admin.guard.ts
@@ -1,13 +1,15 @@
-import { CanActivateFn } from '@angular/router';
+import { CanActivateFn, Router } from '@angular/router';
 import { inject } from '@angular/core';
 import { LoginService } from '../services/login.service';
 import { firstValueFrom } from 'rxjs';
 
 export const adminGuard: CanActivateFn = async (route, state) => {
   const authService = inject(LoginService);
+  const router = inject(Router);
 
   const isLoggedIn = await firstValueFrom(authService.loggedIn$);
   const isAdmin = await firstValueFrom(authService.isAdmin$);
-
-  return isLoggedIn && isAdmin;
+  if(isLoggedIn && isAdmin) return true;
+  await router.navigate(['']);
+  return false;
 };
