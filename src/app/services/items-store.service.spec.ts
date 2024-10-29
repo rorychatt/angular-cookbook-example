d040748afa53ad2ce3884ab0bf6913df2c89c107
rorychatt <pavel.rinne@gmail.com>
2024-10-29 13:47:51 +0100

    info: temp commit

--- src/app/services/items-store.service.spec.ts
+++ src/app/services/items-store.service.spec.ts
@@ -1,6 +1,7 @@
 import { TestBed } from '@angular/core/testing';
 
 import { ItemsStoreService } from './items-store.service';
+import { firstValueFrom } from 'rxjs';
 
 describe('ItemsStoreService', () => {
   let service: ItemsStoreService;
@@ -21,4 +22,20 @@ describe('ItemsStoreService', () => {
       expect(items[0].name).toBe('item1');
     });
   });
+
+  // it('should remove item', async () => {
+  //   service.addItem('item1');
+  //   service.addItem('item2');
+  //
+  //   const items = await firstValueFrom(service.items$);
+  //   const item1Id = items[0].id;
+  //
+  //   expect(item1Id).toBeDefined();
+  //
+  //   service.removeItemById(item1Id);
+  //   service.items$.subscribe((items) => {
+  //     expect(items.length).toBe(1);
+  //     expect(items[0].name).toBe('item2');
+  //   });
+  // });
 });
