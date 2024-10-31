d8e4aa556394b68e3ee2d9cb812b7cbd08f6d964
rorychatt <pavel.rinne@gmail.com>
2024-10-31 10:29:51 +0100

    feat: admin component test remove item by id

--- src/app/admin/admin.component.spec.ts
+++ src/app/admin/admin.component.spec.ts
@@ -3,6 +3,7 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { AdminComponent } from './admin.component';
 import { ActivatedRoute } from '@angular/router';
 import { of } from 'rxjs';
+import { ItemsStoreService } from '../services/items-store.service';
 
 describe('AdminComponent', () => {
   let component: AdminComponent;
@@ -21,6 +22,14 @@ describe('AdminComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should call itemStoreService to remove item by Id', () => {
+    const itemId = 1;
+    const itemsStoreService = TestBed.inject(ItemsStoreService);
+    const spy = spyOn(itemsStoreService, 'removeItemById');
+    component.removeItemById(itemId);
+    expect(spy).toHaveBeenCalledWith(itemId);
   });
 
 });
