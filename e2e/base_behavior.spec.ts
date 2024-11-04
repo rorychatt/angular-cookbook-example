1b40d717b812704d0d9e5f896f12341b7b705f0a
rorychatt <pavel.rinne@gmail.com>
2024-11-04 11:52:24 +0100

    feat: test for login as admin button

--- e2e/base_behavior.spec.ts
+++
@@ -1,7 +0,0 @@
-import { test, expect } from '@playwright/test';
-
-test('has correct title on main page', async ({ page }) => {
-  await page.goto('/');
-
-  await expect(page).toHaveTitle('Angular Cookbook Example');
-})
---
+++ e2e/main_page_tests.spec.ts
@@ -0,0 +1,14 @@
+import { test, expect } from '@playwright/test';
+
+test('has correct title', async ({ page }) => {
+  await page.goto('/');
+
+  await expect(page).toHaveTitle('Angular Cookbook Example');
+});
+
+test('has login as admin button', async ({ page }) => {
+  await page.goto('/');
+
+  await expect(page.locator(`[data-testid='login-admin__button']`)).toBeVisible();
+});
+
