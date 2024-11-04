1aea0d6b081b686e616e7d8214fc27058337abfb
rorychatt <pavel.rinne@gmail.com>
2024-11-04 12:04:33 +0100

    refactor: e2e login to admin page

--- e2e/admin_page.spec.ts
+++ e2e/admin_page.spec.ts
@@ -1,7 +1,15 @@
-import { test, expect } from '@playwright/test';
+import { test, expect, Page } from '@playwright/test';
+import { loginToAdminPage } from './helpers';
 
 test('guard should redirect to main page if direct access', async ({ page }) => {
   await page.goto('/angular-cookbook-example/admin');
 
   await expect(page).toHaveURL('/angular-cookbook-example/');
+});
+
+
+test('says no items in the basket when basket is empty', async ({ page }) => {
+  await loginToAdminPage(page);
+
+  await expect(page.locator(`[data-testid='basket__empty']`)).toBeVisible();
 });
