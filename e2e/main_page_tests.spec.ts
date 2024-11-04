709f0e1113de812d7452b505e279fd79d569eb57
rorychatt <pavel.rinne@gmail.com>
2024-11-04 11:55:12 +0100

    feat: test for login as employee button event

--- e2e/main_page_tests.spec.ts
+++ e2e/main_page_tests.spec.ts
@@ -25,3 +25,11 @@ test('login as admin button navigates to admin page', async ({ page }) => {
 
   await expect(page).toHaveURL('/angular-cookbook-example/admin');
 });
+
+test('login as employee button navigates to admin page', async ({ page }) => {
+  await page.goto('/');
+
+  await page.click(`[data-testid='login-employee__button']`);
+
+  await expect(page).toHaveURL('/angular-cookbook-example/employee');
+});
