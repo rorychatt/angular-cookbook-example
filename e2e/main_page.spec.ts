98b5abfc7cdd78f0ae3fe0b4e1d0dd4b9e4bfcf7
rorychatt <pavel.rinne@gmail.com>
2024-11-05 10:51:01 +0100

    info: remove unnecessary test

--- e2e/main_page.spec.ts
+++ e2e/main_page.spec.ts
@@ -6,18 +6,6 @@ test('has correct title', async ({ page }) => {
   await expect(page).toHaveTitle('Angular Cookbook Example');
 });
 
-test('has login as admin button', async ({ page }) => {
-  await page.goto('/');
-
-  await expect(page.locator(`[data-testid='login-admin__button']`)).toBeVisible();
-});
-
-test('has login as employee button', async ({ page }) => {
-  await page.goto('/');
-
-  await expect(page.locator(`[data-testid='login-employee__button']`)).toBeVisible();
-});
-
 test('login as admin button navigates to admin page', async ({ page }) => {
   await page.goto('/');
 
